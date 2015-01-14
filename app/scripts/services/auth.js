5b5668e298a77000d8b2387bf2bf74d32f5eafae
Scott Lowe <me@scottdlowe.com>
2015-01-14 14:06:38 -0500

    Adding signup and login to auth factory

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -8,16 +8,27 @@
  * Factory in the mhacksFirechatApp.
  */
 angular.module('mhacksFirechatApp')
-  .factory('auth', function () {
-    // Service logic
-    // ...
+  .factory('auth', function ($window, $q) {
 
-    var meaningOfLife = 42;
+    var ref = new $window.Firebase('https://mhacks-firechat-765432.firebaseio.com/');
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      signup: function (user) {
+        var deferred = $q.defer();
+        ref.createUser(user, function (err) {
+          if (err) { deferred.reject(err); }
+          else { deferred.resolve(); }
+        });
+        return deferred.promise;
+      },
+      login: function (user) {
+        var deferred = $q.defer();
+        ref.authWithPassword(user, function (err, auth) {
+          if (err) { deferred.reject(err); }
+          else { deferred.resolve(auth) }
+        })
+        return deferred.promise;
       }
     };
   });
