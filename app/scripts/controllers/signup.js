8a5103d48b109b89fc6f611c1e560b302622b034
Scott Lowe <me@scottdlowe.com>
2015-01-14 11:28:32 -0500

    Generated signup route

---
+++ app/scripts/controllers/signup.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mhacksFirechatApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the mhacksFirechatApp
+ */
+angular.module('mhacksFirechatApp')
+  .controller('SignupCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
